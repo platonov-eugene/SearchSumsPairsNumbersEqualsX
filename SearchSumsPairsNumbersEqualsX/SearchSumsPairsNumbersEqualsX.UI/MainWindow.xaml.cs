5c2f9a6c4f9f2c7a6e6f1221135d0922f689c97f
Platonov Eugene <platonov_eugene@mail.ru>
2016-05-08 23:57:09 +0300

    Реализованы методы корректного ввода данных в элементы управления TextBox.

--- SearchSumsPairsNumbersEqualsX/SearchSumsPairsNumbersEqualsX.UI/MainWindow.xaml.cs
+++ SearchSumsPairsNumbersEqualsX/SearchSumsPairsNumbersEqualsX.UI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,46 +17,74 @@ using System.Windows.Shapes;
 namespace SearchSumsPairsNumbersEqualsX.UI
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Форма приложения
     /// </summary>
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// Конструктор по умолчанию
+        /// Конструктор формы приложения по умолчанию
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void imageHelpTestScenarioNumberOne_MouseEnter(object sender, MouseEventArgs e)
+        private void SetImageOnHelpTestScenario(object sender, MouseEventArgs e)
         {
-            imageHelpTestScenarioNumberOne.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseEnter.png", UriKind.Relative));
+            Image image = sender as Image;
+
+            if (image.Source.ToString() == @"pack://application:,,,/SearchSumsPairsNumbersEqualsX.UI;component/Images/HelpButtonForMouseLeave.png")
+                image.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseEnter.png", UriKind.Relative));
+            else
+                image.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseLeave.png", UriKind.Relative));
         }
 
-        private void imageHelpTestScenarioNumberOne_MouseLeave(object sender, MouseEventArgs e)
+        private void PreviewExecutedCommandCopyCutPaste(object sender, ExecutedRoutedEventArgs e)
         {
-            imageHelpTestScenarioNumberOne.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseLeave.png", UriKind.Relative));
+            if (e.Command == ApplicationCommands.Copy || e.Command == ApplicationCommands.Cut || e.Command == ApplicationCommands.Paste)
+                e.Handled = true;
         }
 
-        private void imageHelpTestScenarioNumberTwo_MouseEnter(object sender, MouseEventArgs e)
+        private void PreviewKeyDownToMatchNumberFormat(object sender, KeyEventArgs e)
         {
-            imageHelpTestScenarioNumberTwo.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseEnter.png", UriKind.Relative));
+            if (e.Key == Key.Space)
+                e.Handled = true;
         }
 
-        private void imageHelpTestScenarioNumberTwo_MouseLeave(object sender, MouseEventArgs e)
+        private void PreviewTextInputToMatchIntegerFormat(object sender, TextCompositionEventArgs e)
         {
-            imageHelpTestScenarioNumberTwo.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseLeave.png", UriKind.Relative));
+            TextBox textBox = sender as TextBox;
+
+            Regex regularExpression = new Regex(@"^[1-9]{1}[\d]*$");
+            if (textBox.SelectionLength == 0)
+                e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
+            else
+                e.Handled = !regularExpression.IsMatch(textBox.Text.Remove(textBox.SelectionStart, textBox.SelectionLength) + e.Text);
         }
 
-        private void imageHelpTestScenarioNumberThree_MouseEnter(object sender, MouseEventArgs e)
+        private void PreviewTextInputToMatchFloatFormat(object sender, TextCompositionEventArgs e)
         {
-            imageHelpTestScenarioNumberThree.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseEnter.png", UriKind.Relative));
-        }
+            TextBox textBox = sender as TextBox;
 
-        private void imageHelpTestScenarioNumberThree_MouseLeave(object sender, MouseEventArgs e)
-        {
-            imageHelpTestScenarioNumberThree.Source = new BitmapImage(new Uri(@"Images/HelpButtonForMouseLeave.png", UriKind.Relative));
+            Regex regularExpression = new Regex(@"^[\-]?[\d]*[\.]?[\d]*$");
+            if (textBox.SelectionLength == 0)
+                e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
+            else
+                e.Handled = !regularExpression.IsMatch(textBox.Text.Remove(textBox.SelectionStart, textBox.SelectionLength) + e.Text);
+
+            if (!e.Handled && textBox.Text.Length != 0)
+                if (textBox.Text + e.Text == "-.")
+                {
+                    textBox.Text = "-0.";
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
+                else if ((textBox.Text == "0" || textBox.Text == "-0") && char.IsDigit(e.Text.Last()))
+                {
+                    textBox.Text = textBox.Text.Substring(0, textBox.Text.Length - 1) + e.Text;
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
         }
     }
 }
