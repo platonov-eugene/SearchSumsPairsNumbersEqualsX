43ffac673e88a8f42d824c66b261cb2ad9690a11
Platonov Eugene <platonov_eugene@mail.ru>
2016-05-09 15:54:19 +0300

    Реализовано формирование коллекции псевдослучайных чисел.

---
+++ SearchSumsPairsNumbersEqualsX/SearchSumsPairsNumbersEqualsX.Logic/RandomExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SearchSumsPairsNumbersEqualsX.Logic
+{
+    /// <summary>
+    /// Класс осуществляющий расширение функциональных возможностей класса System.Random
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Возвращает случайное целое число в указанном диапазоне
+        /// </summary>
+        /// <param name="random">Класс System.Random</param>
+        /// <param name="minimumRandomValue">Минимальное значение заданного диапазона</param>
+        /// <param name="maximumRandomValue">Максимальное значение заданного диапазона</param>
+        /// <returns>Случайное целое число в указанном диапазоне</returns>
+        public static double NextInteger(this Random random, double minimumRandomValue, double maximumRandomValue)
+        {
+            return random.Next((int)minimumRandomValue, (int)maximumRandomValue);
+        }
+
+        /// <summary>
+        /// Возвращает случайное число с плавающей точкой в указанном диапазоне
+        /// </summary>
+        /// <param name="random">Класс System.Random</param>
+        /// <param name="minimumRandomValue">Минимальное значение заданного диапазона</param>
+        /// <param name="maximumRandomValue">Максимальное значение заданного диапазона</param>
+        /// <returns>Случайное число с плавающей точкой в указанном диапазоне</returns>
+        public static double NextDouble(this Random random, double minimumRandomValue, double maximumRandomValue)
+        {
+            return random.NextDouble() * (maximumRandomValue - minimumRandomValue) + minimumRandomValue;
+        }
+
+        /// <summary>
+        /// Возвращает строку случайных чисел в указанном диапазоне разделенных знаком пробела
+        /// </summary>
+        /// <param name="random">Класс System.Random</param>
+        /// <param name="minimumRandomValue">Минимальное значение заданного диапазона</param>
+        /// <param name="maximumRandomValue">Максимальное значение заданного диапазона</param>
+        /// <param name="countNumbers">Необходимое количество сформированных случайных чисел</param>
+        /// <param name="onlyIntegers">Логическое значение указывающее на формирование только целых случайных чисел</param>
+        /// <returns>Строка случайных чисел в указанном диапазоне разделенных знаком пробела</returns>
+        public static string CreateStringRandomNumbers(this Random random, double minimumRandomValue, double maximumRandomValue, int countNumbers, bool onlyIntegers = true)
+        {
+            string stringRandomNumbers = string.Empty;
+
+            for (int i = 1; i <= countNumbers; i++)
+                if (onlyIntegers)
+                    stringRandomNumbers += NextInteger(random, minimumRandomValue, maximumRandomValue) + " ";
+                else
+                    stringRandomNumbers += Math.Round(NextDouble(random, minimumRandomValue, maximumRandomValue), 2) + " ";
+
+            return stringRandomNumbers;
+        }
+    }
+}
--- SearchSumsPairsNumbersEqualsX/SearchSumsPairsNumbersEqualsX.UI/MainWindow.xaml.cs
+++ SearchSumsPairsNumbersEqualsX/SearchSumsPairsNumbersEqualsX.UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SearchSumsPairsNumbersEqualsX.Logic;
 
 namespace SearchSumsPairsNumbersEqualsX.UI
 {
@@ -27,6 +29,7 @@ namespace SearchSumsPairsNumbersEqualsX.UI
         public MainWindow()
         {
             InitializeComponent();
+            textboxSourceCollectionOfNumbers.Focus();
         }
 
         private void SetImageOnHelpTestScenario(object sender, MouseEventArgs e)
@@ -49,6 +52,19 @@ namespace SearchSumsPairsNumbersEqualsX.UI
         {
             if (e.Key == Key.Space)
                 e.Handled = true;
+            if ((e.Key == Key.Left || e.Key == Key.Right || e.Key == Key.Up || e.Key == Key.Down) && Keyboard.Modifiers == ModifierKeys.Shift)
+                e.Handled = true;
+        }
+
+        private void PreviewKeyDownToMatchMultipleNumberFormat(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                if ((sender as TextBox).Text == String.Empty || (sender as TextBox).Text.Last() == ' ')
+                    e.Handled = true;
+            if ((e.Key == Key.Left || e.Key == Key.Right || e.Key == Key.Up || e.Key == Key.Down) && Keyboard.Modifiers == ModifierKeys.Shift)
+                e.Handled = true;
+
+            AddingFinalZeroInMultipleNumberFormat(sender, null);
         }
 
         private void PreviewTextInputToMatchIntegerFormat(object sender, TextCompositionEventArgs e)
@@ -56,10 +72,7 @@ namespace SearchSumsPairsNumbersEqualsX.UI
             TextBox textBox = sender as TextBox;
 
             Regex regularExpression = new Regex(@"^[1-9]{1}[\d]*$");
-            if (textBox.SelectionLength == 0)
-                e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
-            else
-                e.Handled = !regularExpression.IsMatch(textBox.Text.Remove(textBox.SelectionStart, textBox.SelectionLength) + e.Text);
+            e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
         }
 
         private void PreviewTextInputToMatchFloatFormat(object sender, TextCompositionEventArgs e)
@@ -67,15 +80,18 @@ namespace SearchSumsPairsNumbersEqualsX.UI
             TextBox textBox = sender as TextBox;
 
             Regex regularExpression = new Regex(@"^[\-]?[\d]*[\.]?[\d]*$");
-            if (textBox.SelectionLength == 0)
-                e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
-            else
-                e.Handled = !regularExpression.IsMatch(textBox.Text.Remove(textBox.SelectionStart, textBox.SelectionLength) + e.Text);
+            e.Handled = !regularExpression.IsMatch(textBox.Text + e.Text);
 
-            if (!e.Handled && textBox.Text.Length != 0)
+            if (!e.Handled)
                 if (textBox.Text + e.Text == "-.")
                 {
                     textBox.Text = "-0.";
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
+                else if (textBox.Text + e.Text == ".")
+                {
+                    textBox.Text = "0.";
                     textBox.CaretIndex = textBox.Text.Length;
                     e.Handled = true;
                 }
@@ -85,6 +101,130 @@ namespace SearchSumsPairsNumbersEqualsX.UI
                     textBox.CaretIndex = textBox.Text.Length;
                     e.Handled = true;
                 }
+        }
+
+        private void PreviewTextInputToMatchMultipleFloatFormat(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string enteredTextNumber = string.Empty;
+            int indexLastWhitespace = textBox.Text.LastIndexOf(" ");
+
+            if (indexLastWhitespace == -1)
+                enteredTextNumber = textBox.Text;
+            else if (indexLastWhitespace < textBox.Text.Length - 1)
+                enteredTextNumber = textBox.Text.Substring(indexLastWhitespace + 1, textBox.Text.Length - indexLastWhitespace - 1);
+
+            if (enteredTextNumber.Length == 9)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            Regex regularExpression = new Regex(@"^[\-]?[\d]*[\.]?[\d]*$");
+            e.Handled = !regularExpression.IsMatch(enteredTextNumber + e.Text);
+
+            if (!e.Handled)
+                if (enteredTextNumber + e.Text == "-.")
+                {
+                    textBox.Text = textBox.Text.Remove(indexLastWhitespace + 1) + "-0.";
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
+                else if (enteredTextNumber + e.Text == ".")
+                {
+                    textBox.Text = "0.";
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
+                else if ((enteredTextNumber == "0" || enteredTextNumber == "-0") && char.IsDigit(e.Text.Last()))
+                {
+                    enteredTextNumber = enteredTextNumber.Substring(0, enteredTextNumber.Length - 1) + e.Text;
+                    textBox.Text = textBox.Text.Remove(indexLastWhitespace + 1) + enteredTextNumber;
+                    textBox.CaretIndex = textBox.Text.Length;
+                    e.Handled = true;
+                }
+
+            CountNumbersInSourceCollection(sender, e.Handled ? string.Empty : e.Text);
+        }
+
+        private void CountNumbersInSourceCollection(object sender, string inputText = "")
+        {
+            labelCountNumbersInSourceCollection.Content = ((sender as TextBox).Text + inputText).Split(' ').Where(item => item != string.Empty).Count();
+        }
+
+        private void CountNumbersInSourceCollectionByKeyUp(object sender, KeyEventArgs e)
+        {
+            CountNumbersInSourceCollection(sender);
+        }
+
+        private void DisableSelectionTextInTextBoxes(object sender, MouseEventArgs e)
+        {
+            textboxSourceCollectionOfNumbers.SelectionLength = 0;
+            textboxMinimumRandomValue.SelectionLength = 0;
+            textboxMaximumRandomValue.SelectionLength = 0;
+            textboxCountNumbersInRandomCollection.SelectionLength = 0;
+            textboxPredeterminatedNumberX.SelectionLength = 0;
+        }
+
+        private void AddingFinalZeroInNumberFormat(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if ((sender as TextBox).Text.LastOrDefault() == '.')
+            {
+                textBox.Text += "0";
+                textBox.CaretIndex = textBox.Text.Length;
+            }
+        }
+
+        private void AddingFinalZeroInMultipleNumberFormat(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string enteredTextNumber = string.Empty;
+            int indexLastWhitespace = textBox.Text.LastIndexOf(" ");
+
+            if (indexLastWhitespace == -1)
+                enteredTextNumber = textBox.Text;
+            else if (indexLastWhitespace < textBox.Text.Length - 1)
+                enteredTextNumber = textBox.Text.Substring(indexLastWhitespace + 1, textBox.Text.Length - indexLastWhitespace - 1);
+
+            if (enteredTextNumber.LastOrDefault() == '.')
+            {
+                textBox.Text += "0";
+                textBox.CaretIndex = textBox.Text.Length;
+            }
+        }
+
+        private void CreateRandomCollection(object sender, RoutedEventArgs e)
+        {
+            textboxMinimumRandomValue.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFABADB3"));
+            textboxMaximumRandomValue.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFABADB3"));
+            textboxCountNumbersInRandomCollection.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFABADB3"));
+
+            if (textboxMinimumRandomValue.Text.Length == 0)
+                textboxMinimumRandomValue.BorderBrush = Brushes.Red;
+            else if (textboxMaximumRandomValue.Text.Length == 0)
+                textboxMaximumRandomValue.BorderBrush = Brushes.Red;
+            else if (textboxCountNumbersInRandomCollection.Text.Length == 0)
+                textboxCountNumbersInRandomCollection.BorderBrush = Brushes.Red;
+            else if (Convert.ToDouble(textboxMinimumRandomValue.Text, new CultureInfo("en-EN")) > Convert.ToDouble(textboxMaximumRandomValue.Text, new CultureInfo("en-EN")))
+            {
+                textboxMinimumRandomValue.BorderBrush = Brushes.Red;
+                textboxMaximumRandomValue.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                this.Cursor = Cursors.Wait;
+                double minimumRandomValue = Convert.ToDouble(textboxMinimumRandomValue.Text, new CultureInfo("en-EN"));
+                double maximumRandomValue = Convert.ToDouble(textboxMaximumRandomValue.Text, new CultureInfo("en-EN"));
+                int countNumbers = Convert.ToInt32(textboxCountNumbersInRandomCollection.Text, new CultureInfo("en-EN"));
+                bool onlyIntegers = radiobuttonOnlyIntegers.IsChecked == true;
+
+                textboxSourceCollectionOfNumbers.Text = new Random().CreateStringRandomNumbers(minimumRandomValue, maximumRandomValue, countNumbers, onlyIntegers);
+                CountNumbersInSourceCollection(textboxSourceCollectionOfNumbers);
+                this.Cursor = Cursors.Arrow;
+            }
         }
     }
 }
